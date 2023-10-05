4962a59289ae059ee33b6df241a81157b5c643e0
José Vilmar Estácio de Souza <vilmar@informal.com.br>
2023-10-05 19:19:56 -0300

    Incluir TO para calc.

--- src/main/java/com/example/demo/controllers/CalcController.java
+++ src/main/java/com/example/demo/controllers/CalcController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.services.CalcService;
+import com.example.demo.to.CalcTO;
 
 @RestController
 public class CalcController {
@@ -13,7 +15,9 @@ public class CalcController {
     private CalcService calcService;
 
     @GetMapping("/calc")
-    public int calculate(@RequestParam("operand") String operand, @RequestParam("a") int a, @RequestParam("b") int b) {
-        return calcService.calc(operand, a, b);
+    public ResponseEntity<CalcTO> calculate(@RequestParam("operand") String operand, @RequestParam("a") int a, @RequestParam("b") int b) {
+        int result = calcService.calc(operand, a, b);
+        CalcTO calcTO = new CalcTO(operand, a, b, result);
+        return ResponseEntity.ok(calcTO);
     }
 }
---
+++ src/main/java/com/example/demo/to/CalcTO.java
@@ -0,0 +1,5 @@
+package com.example.demo.to;
+
+public record CalcTO(String operand, int operand1, int operand2, int result) {
+
+}
