bd6c9d29e8ff999f5207f8f89fa58d26799b4609
José Vilmar Estácio de Souza <vilmar@informal.com.br>
2023-10-05 19:00:11 -0300

    incluir servico, primeira versao.

--- src/main/java/com/example/demo/controllers/CalcController.java
+++ src/main/java/com/example/demo/controllers/CalcController.java
@@ -4,28 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.services.CalcService;
+
 @RestController
 public class CalcController {
+    private CalcService calcService;
 
     @GetMapping("/calc")
     public int calculate(@RequestParam("operand") String operand, @RequestParam("a") int a, @RequestParam("b") int b) {
-        int result = 0;
-        switch (operand) {
-            case "add":
-                result = a + b;
-                break;
-            case "sub":
-                result = a - b;
-                break;
-            case "mul":
-                result = a * b;
-                break;
-            case "div":
-                result = a / b;
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid operand: " + operand);
-        }
-        return result;
+        calcService = new CalcService();
+        return calcService.calc(operand, a, b);
     }
 }
---
+++ src/main/java/com/example/demo/services/CalcService.java
@@ -0,0 +1,24 @@
+package com.example.demo.services;
+
+public class CalcService {
+    public int calc(String operand, int a, int b) {
+        int result = 0;
+        switch (operand) {
+            case "add":
+                result = a + b;
+                break;
+            case "sub":
+                result = a - b;
+                break;
+            case "mul":
+                result = a * b;
+                break;
+            case "div":
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid operand: " + operand);
+        }
+        return result;
+    }
+}
