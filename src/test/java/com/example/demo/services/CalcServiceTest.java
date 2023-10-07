0b0f7b748b91abc4f62329c807e8662077c855a7
José Vilmar Estácio de Souza <vilmar@informal.com.br>
2023-10-07 08:29:21 -0300

    usando mock e when.

--- src/test/java/com/example/demo/services/CalcServiceTest.java
+++ src/test/java/com/example/demo/services/CalcServiceTest.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,24 +19,28 @@ public class CalcServiceTest {
 
     @Test
     public void testCalcAdd() {
+        when(xcalcService.add(1,2)).thenReturn(3);
         int result = service.calc("add", 1, 2);
         assertEquals(3, result);
     }
 
     @Test
     public void testCalcSub() {
+            when(xcalcService.sub(1,2)).thenReturn(-1);
         int result = service.calc("sub", 1, 2);
         assertEquals(-1, result);
     }
 
     @Test
     public void testCalcMul() {
+        when(xcalcService.mul(1,2)).thenReturn(2);
         int result = service.calc("mul", 1, 2);
         assertEquals(2, result);
     }
 
     @Test
     public void testCalcDiv() {
+        when(xcalcService.div(1,2)).thenReturn(0);  
         int result = service.calc("div", 1, 2);
         assertEquals(0, result);
     }
